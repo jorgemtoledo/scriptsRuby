19fbb9f836a1fb157f381ee43dac138ab24b584e
jorgemtoledo <jorge.toledo@gmail.com>
2018-11-03 23:36:41 -0400

    Authentication

--- ruby_studies/list_todo/app/controllers/application_controller.rb
+++ ruby_studies/list_todo/app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :authentication
+
+  private
+
+  def authentication
+    authenticate_or_request_with_http_basic  do |username, password|
+      # username == 'xxxx' && password == 'yyyy'
+      username == Rails.application.credentials.authentication[:username] && password == Rails.application.credentials.authentication[:password]
+    end
+  end
 end
